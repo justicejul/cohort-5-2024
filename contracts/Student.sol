9d0328b2620790591be5d6a76a4858fe90d21e5a
Catherine Jonathan <35270183+kateberryd@users.noreply.github.com>
2024-08-03 13:53:00 +0100

    feat: struct

---
+++ contracts/Student.sol
@@ -0,0 +1,8 @@
+ // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+ struct Student {
+        address studentAddr;
+        string name;
+        uint256 studentId;
+        uint8 age;
+    }
