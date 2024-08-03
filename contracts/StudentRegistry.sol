e82e4f5c93299bd6b10c4ab5eac3db6de84624d9
Catherine Jonathan <35270183+kateberryd@users.noreply.github.com>
2024-08-03 13:51:16 +0100

    refac: refactor contract

--- contracts/StudentRegistry.sol
+++ contracts/StudentRegistry.sol
@@ -1,32 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+import "./Ownable.sol";
+import "./Student.sol";
 
-contract StudentRegistry {
+
+contract StudentRegistry is Ownable {
+    //custom erros
+    error NameIsEmpty();
+    error UnderAge(uint8 age, uint8 expectedAge);
+
     //custom data type
-    struct Student {
-        address studentAddr;
-        string name;
-        uint256 studentId;
-        uint8 age;
-    }
-
-
-    address public  owner;
-
-
-    constructor () {
-        owner = msg.sender;
-    }
-
+   
+  
     //dynamic array of students
     Student[] private students;
+
+    mapping(address => Student) public studentsMapping;
+
+
+    modifier isNotAddressZero() {
+        require(msg.sender != address(0), "Invalid Address");
+        _;
+    }
 
     function addStudent(
         address _studentAddr,
         string memory _name,
         uint8 _age
-    ) public {
-        require(owner == msg.sender, "You are not authorized");
+    ) public  isNotAddressZero {
+        if (bytes(_name).length == 0) {
+            revert NameIsEmpty();
+        }
+
+        if (_age < 18) {
+            revert UnderAge({age: _age, expectedAge: 18});
+        }
+
         uint256 _studentId = students.length + 1;
         Student memory student = Student({
             studentAddr: _studentAddr,
@@ -36,12 +45,52 @@ contract StudentRegistry {
         });
 
         students.push(student);
+        // add student to studentsMapping
+        studentsMapping[_studentAddr] = student;
     }
 
-    function getStudent(uint8 _studentId) public view returns (Student memory) {
+    function getStudent(uint8 _studentId)
+        public
+        view
+        isNotAddressZero
+        returns (Student memory)
+    {
         return students[_studentId - 1];
     }
 
+    function getStudentFromMapping(address _studentAddr)
+        public
+        view
+        isNotAddressZero
+        returns (Student memory)
+    {
+        return studentsMapping[_studentAddr];
+    }
+
+    function deleteStudent(address _studentAddr)
+        public
+        onlyOwner
+        isNotAddressZero
+    {
+        require(
+            studentsMapping[_studentAddr].studentAddr != address(0),
+            "Student does not exist"
+        );
+
+        // delete studentsMapping[_studentAddr];
+
+        Student memory student = Student({
+            studentAddr: address(0),
+            name: "",
+            age: 0,
+            studentId: 0
+        });
+
+        studentsMapping[_studentAddr] = student;
+    }
 
 
+    function modifyOwner(address _newOwner) public {
+        changeOwner(_newOwner);
+    }
 }
